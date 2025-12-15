8b55965ddf75b2fe994a5a25dba7da5efa8c8012
Mariozalac716 <zalac.mario1@gmail.com>
2025-12-15 14:06:45 +0100

    Repository update

--- src/main/java/se/chasacademy/databaser/jpastart/repositories/CourseRepository.java
+++ src/main/java/se/chasacademy/databaser/jpastart/repositories/CourseRepository.java
@@ -1,5 +1,7 @@
 package se.chasacademy.databaser.jpastart.repositories;
 
+import se.chasacademy.databaser.jpastart.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
